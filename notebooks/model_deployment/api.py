de1ee59c374286956ac3092b14ad80f4d5b716d3
Daniel Rojas <danielmrp93@gmail.com>
2019-02-11 20:43:22 -0500

    cambio de puerto a 8888
    cambio de puerto

--- notebooks/model_deployment/api.py
+++ notebooks/model_deployment/api.py
@@ -42,4 +42,4 @@ class PhishingApi(Resource):
     
     
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
